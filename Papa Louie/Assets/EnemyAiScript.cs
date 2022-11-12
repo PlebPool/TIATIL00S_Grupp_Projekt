043c0f4c0fe02c1af71f0cc40805a96bf0602cc1
Mattias Wegblad <mattias.h.wegblad@gmail.com>
2022-11-12 22:50:16 +0100

    ivison

--- Papa Louie/Assets/EnemyAiScript.cs
+++ Papa Louie/Assets/EnemyAiScript.cs
@@ -12,7 +12,9 @@ public class EnemyAiScript : MonoBehaviour
 
     public Transform player;
 
-    public LayerMask whatIsGround, whatIsPlayer;
+    public LayerMask whatIsPlayer, whatIsGround, whatAmI;
+    private int _playerLayer;
+    private int _groundLayer;
 
     public Vector3 walkPoint;
     private bool _walkPointSet;
@@ -26,6 +28,14 @@ public class EnemyAiScript : MonoBehaviour
 
     public float fov;
 
+    private bool _seeingPlayer = false;
+
+    private void Start()
+    {
+        _playerLayer = (int)(Mathf.Log(whatIsPlayer) / Mathf.Log(2));
+        _groundLayer = (int)(Mathf.Log(whatIsGround) / Mathf.Log(2));
+    }
+
     private void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
@@ -36,33 +46,44 @@ public class EnemyAiScript : MonoBehaviour
         var t = transform;
         var pos = t.position;
 
-        var fwd = t.forward * 10;
-
         playerInSightRange = Physics.CheckSphere(pos, 10, whatIsPlayer);
         playerInAttackRange = Physics.CheckSphere(pos, 1, whatIsPlayer);
-
         var targetDir = player.position - t.position;
         var angle = Vector3.Angle(targetDir, t.forward);
-        var rayToPlayer = false;
+        var seesPlayer = false;
         if (angle < fov && playerInSightRange)
         {
             Debug.DrawLine(pos, player.position, Color.red);
-            rayToPlayer = Physics.Raycast(pos, targetDir, whatIsPlayer);
+            if (Physics.Raycast(pos, targetDir, out var hit, Mathf.Infinity, ~whatAmI))
+            {
+                seesPlayer = hit.transform.gameObject.layer == _playerLayer;
+            }
         }
-        
         Debug.DrawRay(pos, t.forward * 10, Color.gray);
-        
-        switch (rayToPlayer)
+        if (seesPlayer)
         {
-            case false when !playerInAttackRange:
-                // Patrol();
-                break;
-            case true when !playerInAttackRange:
-                // Chase();
-                break;
+            _seeingPlayer = true;
+            if (playerInAttackRange)
+            {
+                Attack();
+            }
+            else if (playerInSightRange)
+            {
+                Chase();
+            }
         }
-
-        if (rayToPlayer && playerInAttackRange) Attack();
+        else
+        {
+            if (_seeingPlayer)
+            {
+                _seeingPlayer = false;
+                walkPoint = player.position;
+            }
+            else
+            {
+                Patrol();
+            }
+        }
     }
 
     private void Patrol()
@@ -89,8 +110,6 @@ public class EnemyAiScript : MonoBehaviour
         // Checking if walk point is on ground.
         if (Physics.Raycast(walkPoint, -transform.up, 2f, whatIsGround))
             _walkPointSet = true;
-        
-        
     }
 
     private void Chase()
